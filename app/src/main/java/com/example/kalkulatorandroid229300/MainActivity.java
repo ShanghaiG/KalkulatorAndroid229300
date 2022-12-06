7d7519d6652f83b8cd022eb699383a19bd94fa74
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2022-12-06 20:46:39 +0100

    Init project

---
+++ app/src/main/java/com/example/kalkulatorandroid229300/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kalkulatorandroid229300;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
